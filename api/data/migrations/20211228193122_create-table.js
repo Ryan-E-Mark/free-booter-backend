234d696751f616b28939830c2783dcb8d2eb74ab
Ryan Mark <rymark907@gmail.com>
2021-12-28 10:41:10 -0900

    setting up heroku

---
+++ api/data/migrations/20211228193122_create-table.js
@@ -0,0 +1,14 @@
+
+exports.up = async (knex) => {
+  await knex.schema
+    .createTable('boards', (boards) => {
+        boards.increments('board_id')
+        boards.string('board_name', 200).notNullable()
+        boards.string('board_size', 200).notNullable()
+        boards.string('in_stock').notNullable()
+    })
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('boards')
+};
