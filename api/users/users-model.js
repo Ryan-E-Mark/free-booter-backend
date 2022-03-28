50ba0f2fd38d6e81dfec5ba7026d15214e71f0f9
Ryan Mark <rymark907@gmail.com>
2022-03-28 14:13:20 -0700

    adding endpoints for users router

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -5,7 +5,25 @@ async function getAll() {
     return result;
 }
 
+async function getById(id) {
+    const result = await db('users').where('user_id', id);
+    return result;
+}
+
+async function addUser(user) {
+    const result = await db('users').insert(user, ['user_id', 'username', 'email']);
+    return result[0];
+}
+
+async function updateById(id, user) {
+    const resultId = await db('users').where('user_id', id).update(user);
+    return await db('users').where('user_id', resultId).first();
+}
+
 
 module.exports = {
     getAll,
+    getById,
+    addUser,
+    updateById
 }
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Users = require('./users-model');
 const bcrypt = require('bcryptjs');
+const buildToken = require('./token-builder');
 
 
 const router = express();
@@ -23,9 +24,21 @@ router.post('/login', async (req, res, next) => {
     if (!valid) {
         next({status: 404, message: "invalid credentials"});
     } else {
-        res.status(200).json({user_id, username});
+        const token = buildToken(req.user);
+        res.status(200).json({user_id, username, token});
     }
-})
+});
 
+router.put('/:id', async (req, res, next) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 6);
+    user.password = hash;
+    try {
+        const updatedUser = await Users.updateById(req.params.id, user);
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = router;
